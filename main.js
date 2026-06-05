4232a5c05ea1b5bb9b21868ce79ba3a5c8b2b218
Edqwird <161136059+Edqwird@users.noreply.github.com>
2026-06-06 00:38:19 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ document.querySelectorAll('.up_num').forEach(button => {
 
         const numBox = this.closest('.num_box');
 
+        const numQuestion = numBox.querySelector('.num_question');
+        
+        if (numBox.classList.contains('van')) {
+            numQuestion.textContent = numQuestion.textContent.replace(' Задание', '');
+        } else {
+            if (!numQuestion.textContent.includes('задание')) {
+                numQuestion.textContent = numQuestion.textContent + ' Задание';
+            }
+        }
+
         numBox.classList.toggle('van');
     });
+});
+
+
+
+document.querySelectorAll('#tk_q').forEach(el => {
+    if (el.textContent.includes('Редактировать')) {
+        el.addEventListener('click', () => {
+            document.querySelector('.ege_pr').style.display = 'flex';
+            document.querySelector('.dow_form').style.display = 'flex';
+            el.style.backgroundColor = 'rgb(240, 240, 240)';
+        });
+    }
 });

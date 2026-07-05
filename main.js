e67ab96dd8b4d2a904e5d94d99f3a9638cebe883
Edqwird <161136059+Edqwird@users.noreply.github.com>
2026-07-05 19:00:47 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,98 @@
-document.querySelectorAll('.up_num').forEach(button => {
-    button.addEventListener('click', function(event) {
-        event.stopPropagation();
-
-        const numBox = this.closest('.num_box');
-
-        const numQuestion = numBox.querySelector('.num_question');
-        
-        if (numBox.classList.contains('van')) {
-            numQuestion.textContent = numQuestion.textContent.replace(' Задание', '');
+document.addEventListener('DOMContentLoaded', function() {
+    // document.querySelector('body').style.height = '100vh';
+    const text = 'questor';
+    const logo = document.querySelector('.logo_start');
+    logo.textContent = '';
+    let i = 0;
+    
+    function typeSec() {
+        if (i < text.length) {
+            logo.textContent += text[i];
+            i++
+            setTimeout(typeSec, 300);
         } else {
-            if (!numQuestion.textContent.includes('задание')) {
-                numQuestion.textContent = numQuestion.textContent + ' Задание';
-            }
+            setTimeout(typeAnima, 700);
         }
+    }
 
-        numBox.classList.toggle('van');
-    });
+    typeSec();
+
+    function typeAnima() {
+        logo.classList.add('animate');
+        // setTimeout(() => {
+        //     document.querySelector('.sky_title').style.height = '100px';
+        //     document.querySelector('.logo_start').style.top = '40%';
+        // }, 3000);
+        setTimeout(() => {
+            document.querySelector('.boardP').classList.add('animate');
+        }, 3200);
+        setTimeout(() => {
+            document.querySelector('.one_title').classList.add('animate');
+        }, 3500);
+        setTimeout(() => {
+            document.querySelector('.two_title').classList.add('animate');
+        }, 3800);
+        setTimeout(() => {
+            document.querySelector('.three_title').classList.add('animate');
+        }, 4200);
+        setTimeout(() => {
+            document.querySelector('.four_title').classList.add('animate');
+        }, 4600);
+        setTimeout(() => {
+            // document.querySelector('.end_title').style.display = 'block';
+            document.querySelector('.fife_title').classList.add('animate');
+        }, 5200);
+    }
 });
 
 
+const bL = document.querySelector('.circleL');
+const bR = document.querySelector('.circleR');
+const cardSel = document.querySelector('.cardSel');
+const cardWidth = 460;
 
-document.querySelectorAll('#tk_q').forEach(el => {
-    if (el.textContent.includes('Редактировать')) {
-        el.addEventListener('click', () => {
-            document.querySelector('.ege_pr').style.display = 'flex';
-            document.querySelector('.dow_form').style.display = 'flex';
-            el.style.backgroundColor = 'rgb(240, 240, 240)';
-        });
-    }
+bR.addEventListener('click', function() {
+    cardSel.scrollBy({left: cardWidth, behavior: "smooth"});
+    document.querySelector('.artL').style.opacity = '100%';
+    document.querySelector('.circleR').style.cursor = 'default';
+    document.querySelector('.circleL').style.cursor = 'pointer';
+    document.querySelector('.artR').style.opacity = '60%';
+
+});
+
+bL.addEventListener('click', function() {
+    cardSel.scrollBy({left: -cardWidth, behavior: "smooth"});
+    document.querySelector('.artL').style.opacity = '60%';
+    document.querySelector('.circleL').style.cursor = 'default';
+    document.querySelector('.circleR').style.cursor = 'pointer';
+    document.querySelector('.artR').style.opacity = '100%';
+});
+
+
+document.querySelector('.boxesCircle').addEventListener('click', function() {
+    this.classList.remove('animate');
+    void this.offsetHeight;
+    this.classList.add('animate');
+});
+
+
+document.querySelectorAll('.cir_dop').forEach(btn => {
+   btn.addEventListener('click', function() {
+        const questBox = this.closest('.quecolB');
+        const textInf = questBox.querySelector('.textInfo');
+
+        if (textInf.style.display === 'block') {
+            textInf.style.display = 'none';
+            this.textContent = '+';
+        } else {
+            document.querySelectorAll('.textInfo').forEach(el => {
+                el.style.display = 'none';
+            });
+            document.querySelectorAll('.cir_dop').forEach(but => {
+             but.textContent = '+';
+            });
+            textInf.style.display = 'block';
+            this.textContent = '-';
+        }
+   }); 
 });

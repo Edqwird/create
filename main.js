85ff50b4caefeea0e219b910bbaa30a3c5adc00d
Edqwird <161136059+Edqwird@users.noreply.github.com>
2026-07-14 19:38:26 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,81 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const text = 'questor';
-    const logo = document.querySelector('.logo_start');
-    logo.textContent = '';
-    let i = 0;
     
-    function typeSec() {
-        if (i < text.length) {
-            logo.textContent += text[i];
-            i++
-            setTimeout(typeSec, 300);
-        } else {
-            setTimeout(typeAnima, 700);
-        }
-    }
-
-    typeSec();
-
-    function typeAnima() {
-        logo.classList.add('animate');
-        // setTimeout(() => {
-        //     document.querySelector('.sky_title').style.height = '100px';
-        //     document.querySelector('.logo_start').style.top = '40%';
-        // }, 3000);
+    setTimeout(() => {
+        document.querySelector('.one_title').classList.add('animate');
+    }, 1100);
+    setTimeout(() => {
+        document.querySelector('.two_title').classList.add('animate');
+    }, 1800);
+    // setTimeout(() => {
+    //     document.querySelector('.three_title').classList.add('animate');
+    // }, 2200);
         setTimeout(() => {
-            document.querySelector('.boardP').classList.add('animate');
-        }, 3200);
-        setTimeout(() => {
-            document.querySelector('.one_title').classList.add('animate');
-        }, 3500);
-        setTimeout(() => {
-            document.querySelector('.two_title').classList.add('animate');
-        }, 3800);
-        setTimeout(() => {
-            document.querySelector('.three_title').classList.add('animate');
-        }, 4200);
-        setTimeout(() => {
-            document.querySelector('.four_title').classList.add('animate');
-        }, 4200);
-        setTimeout(() => {
-            // document.querySelector('.end_title').style.display = 'block';
-            document.querySelector('.fife_title').classList.add('animate');
-        }, 4200);
-        setTimeout(() => {
-            document.querySelector('.end_title').classList.add('animate');
-        }, 4200);
-    }
+        document.querySelector('.four_title').classList.add('animate');
+    }, 2200);
+    setTimeout(() => {
+        document.querySelector('.fife_title').classList.add('animate');
+    }, 2200);
+    setTimeout(() => {
+        document.querySelector('.end_title').classList.add('animate');
+    }, 2200);
 });
 
+// const img = document.querySelector(".imgHum");
+// const oldSrc = 'mar.png';
+// const newSrc = 'mar1.png';
 
-const bL = document.querySelector('.circleL');
-const bR = document.querySelector('.circleR');
-const cardSel = document.querySelector('.cardSel');
-const cardWidth = 460;
-
-// bR.addEventListener('click', function() {
-//     cardSel.scrollBy({left: cardWidth, behavior: "smooth"});
-//     document.querySelector('.artL').style.opacity = '100%';
-//     document.querySelector('.circleR').style.cursor = 'default';
-//     document.querySelector('.circleL').style.cursor = 'pointer';
-//     document.querySelector('.artR').style.opacity = '60%';
-
+// img.addEventListener('mouseenter', () => {
+//     img.src = 'mar1.png';
 // });
 
-// bL.addEventListener('click', function() {
-//     cardSel.scrollBy({left: -cardWidth, behavior: "smooth"});
-//     document.querySelector('.artL').style.opacity = '60%';
-//     document.querySelector('.circleL').style.cursor = 'default';
-//     document.querySelector('.circleR').style.cursor = 'pointer';
-//     document.querySelector('.artR').style.opacity = '100%';
+// img.addEventListener('mouseleave', () => {
+//     img.src = 'mar.png';
 // });
 
 
-document.querySelector('.boxesCircle').addEventListener('click', function() {
-    this.classList.remove('animate');
-    void this.offsetHeight;
-    this.classList.add('animate');
-});
+// document.querySelector('.boxesCircle').addEventListener('click', function() {
+//     this.classList.remove('animate');
+//     void this.offsetHeight;
+//     this.classList.add('animate');
+// });
 
 
 document.querySelectorAll('.cir_dop').forEach(btn => {
@@ -100,44 +62,25 @@ document.querySelectorAll('.cir_dop').forEach(btn => {
 });
 
 
-window.addEventListener("scroll", function() {
-    const el = document.querySelector('.two_title');
-    const elTop = el.offsetTop;
-    const board = document.querySelector('.boardP');
+// window.addEventListener("scroll", function() {
+//     const el = document.querySelector('.add_title');
+//     const elTop = el.offsetTop;
+//     const board = document.querySelector('.sky_box');
 
-    if (window.scrollY >= elTop) {
-        board.style.position = 'fixed';
-        if (!board.classList.contains('animatest')) {
-            board.classList.remove('animateOut');
-            board.classList.remove('animate');
-            board.classList.add('animatest');
-        }
-    } else {
-        if (board.classList.contains('animatest')) {
-            board.classList.remove('animatest');
-            board.classList.add('animateOut');
-            setTimeout(() => {
-                board.style.position = 'static';
-                board.classList.remove('animateOut');
-                board.classList.add('animate');
-            }, 400);
-        }
-    }
-});
-
-const navbar = document.querySelector('.boardP');
-const sections = document.querySelector('.four_title');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            navbar.style.background = '#b8b8b894';
-            navbar.style.transition = 'background 1s ease';
-        } else {
-            navbar.style.background = '#e0dfe294';
-            navbar.style.transition = 'background 1s ease';
-        }
-    });
-});
-
-observer.observe(sections);
+//     if (window.scrollY >= elTop) {
+//         board.style.position = 'fixed';
+//         if (!board.classList.contains('animatest')) {
+//             board.classList.remove('animateOut');
+//             board.classList.add('animatest');
+//         }
+//     } else {
+//         if (board.classList.contains('animatest')) {
+//             board.classList.remove('animatest');
+//             board.classList.add('animateOut');
+//             setTimeout(() => {
+//                 board.style.position = 'relative';
+//                 board.classList.remove('animateOut');
+//             }, 400);
+//         }
+//     }
+// });
